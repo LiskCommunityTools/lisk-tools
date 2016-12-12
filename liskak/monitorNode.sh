3ee734758463aabc94d2ddf889b31d536cf5749c
Isabello <isabella@lisk.io>
2016-12-11 23:35:06 -0600

    Remove folders to use submodules instead

--- liskak/monitorNode.sh
+++
@@ -1,8 +0,0 @@
-RELEASE="test"
-LISK_PATH="/home/$USER/lisk-$RELEASE"
-LISK_LOG="/home/$USER/lisk-$RELEASE/logs/lisk_$RELEASE.app.log"
-LISK_SH="/home/$USER/lisk-$RELEASE/lisk.sh"
-LOG_FILE="logs/monitorNode.log"
-MINUTES="5"
-pkill -f $LISK_LOG -9
-nohup bash liskak.sh -S ${LISK_PATH} -J $LISK_LOG -K $LISK_SH -B $MINUTES  > $LOG_FILE 2>&1&
